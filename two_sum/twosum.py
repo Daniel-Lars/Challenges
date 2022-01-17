31487fdaa6611b3d2cd6436f24020db464cfaa51
Daniel Wendel <daniel2wendel@gmail.com>
2022-01-17 17:02:35 +0100

    twosum solution

---
+++ two_sum/twosum.py
@@ -0,0 +1,13 @@
+# Sum of two: Given a list of integers and a target:
+# return the list index of the 2 integers, that added up equals the target
+
+nums = [3,3]
+target = 6
+
+for idx, a in enumerate(nums):
+    for b in nums[idx + 1:]:
+        if sum((a,b)) == target:
+            if a != b:
+                print([idx, nums.index(b)])
+            else:
+                print([idx, nums.index(b,idx+1)])
